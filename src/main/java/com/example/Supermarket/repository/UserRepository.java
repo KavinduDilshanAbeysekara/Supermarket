a4f477429a1eb2b2912e4bd25fc037b8ee823d7b
KavinduDilshanAbeysekara <kavindudilshan735@gmail.com>
2024-12-25 10:40:46 +0530

    user repository

--- src/main/java/com/example/Supermarket/repository/UserRepository.java
+++ src/main/java/com/example/Supermarket/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.example.Supermarket.repository;
 
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.Supermarket.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer>{
-    
+public interface UserRepository extends JpaRepository<User, Long> {
+    //to find user by username
+    Optional<User> findByUsername(String username);
 }
