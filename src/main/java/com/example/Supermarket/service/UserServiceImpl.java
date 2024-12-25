4b8b77075963ac8c590130a61292ef9e4050db55
KavinduDilshanAbeysekara <kavindudilshan735@gmail.com>
2024-12-25 10:53:29 +0530

    userserviceimpl

--- src/main/java/com/example/Supermarket/service/UserServiceImpl.java
+++ src/main/java/com/example/Supermarket/service/UserServiceImpl.java
@@ -1,75 +1,29 @@
 package com.example.Supermarket.service;
 
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.stereotype.Service;
 
-import com.example.Supermarket.entity.Order;
 import com.example.Supermarket.entity.User;
 import com.example.Supermarket.repository.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
-    private final UserRepository userRepository;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private UserRepository userRepository;
+
+    @Autowired
+    //private PasswordEncoder passwordEncoder;
 
     @Override
-    public List<User> findAllUsers() {
-        return userRepository.findAll();
-    }
+    public User createUser(User user) {
+        //user.setPassword(passwordEncoder.encode(user.getPassword())); //encode the password, so that it is not human readable
 
-    @Override
-    public User addNewUser(User user) {
         return userRepository.save(user);
     }
-
-    @Override
-    public void deleteUserById(Integer userId) {
-
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("No such user found against this ID"));
-
-        user.setOrders(null);
-        userRepository.delete(user);
-    }
-
-    @Override
-    public void deleteUser(User user) {
-        if (userRepository.existsById(user.getId())) {
-            userRepository.delete(user);
-        } else {
-            throw new IllegalArgumentException("No such user found in the database");
-        }
-    }
-
-    @Override
-    public User updateUser(User updatedUser) {
-
-        userRepository.findById(updatedUser.getId())
-                .orElseThrow(() -> new IllegalArgumentException("No such user found against this ID"));
-
-        return userRepository.save(updatedUser);
-    }
-
-    @Override
-    public User getUserById(Integer userId) {
-        return userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("No such user found within the database"));
-    }
-
-    @Override
-    public List<Order> getOrdersByUserId(Integer userId) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("No such user found within the database"));
-        List<Order> orders = user.getOrders();
-        if (orders.isEmpty()) {
-            throw new IllegalStateException("No orders made yet by this user");
-        }
-        return orders;
-    }
+    
 }
