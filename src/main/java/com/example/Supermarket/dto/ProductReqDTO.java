b98e24efacf04bf57814530bd224018f25d49788
Ruwangi19 <ruwangipraboda@gmail.com>
2025-01-23 13:43:11 +0530

    create new DTO

--- src/main/java/com/example/Supermarket/dto/ProductReqDTO.java
+++ src/main/java/com/example/Supermarket/dto/ProductReqDTO.java
@@ -1,5 +1,14 @@
-package com.example.Supermarket.dto;
+package com.ijse.springintro.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ProductReqDTO {
-    
+    private String name;
+    private Double price;
+    private String description;
+    private Long categoryId;
 }
+
